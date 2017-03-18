5f1d35490ad1fb7bce6bb1b113ce877bdad90596
Adam Lezotte <adam@lezotte.me>
2017-03-18 16:19:34 -0400

    three sets of radio buttons, module, and controller

---
+++ script.js
@@ -0,0 +1,9 @@
+var app = angular.module("badaBingBadaBoom", []);
+
+app.controller("adoptCtrl", function($scope){
+    
+    $scope.sex = "";
+    $scope.age = "";
+    $scope.size = "";
+    
+});
