11774c9a98b940cc454a42095191374076342b68
Jay Fincher <fincher_jay@yahoo.com>
2017-03-21 20:53:53 -0400

    updated module

--- script/module.js
+++ script/module.js
@@ -1,1 +1,35 @@
 var app = angular.module("badaBingBadaBoom", ["ngRoute"]);
+
+app.config(function($routeProvider, $locationProvider){ 
+
+	$routeProvider 
+
+		.when('/form', {
+
+			controller: 'formCtrl', 
+
+			templateUrl: 'view/form.html'
+
+		})
+
+		.when('/result', {
+
+			controller: 'resultCtrl',
+
+			templateUrl: 'view/result.html'
+
+		})
+
+		.when('/adopt', {
+
+			controller: 'adoptCtrl',
+
+			templateUrl: 'view/adopt.html'
+
+		});
+
+	$locationProvider.hashPrefix(''); 
+});
+
+
+// form, adopt, result
