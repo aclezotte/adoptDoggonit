b6cff8eb9219609e9373b9775b42c604af73bac4
Adam Lezotte <adam@lezotte.me>
2017-03-21 20:11:40 -0400

    clickable labels

--- factory.js
+++ factory.js
@@ -9,7 +9,7 @@ app.factory("adoptFactory", function($http){
         getPets: function(sex, age, size) {
                 var promise = $http({
                     method: "GET",
-                    url: "http://api.petfinder.com/pet.find?key=ad9636f9b9a6415e1aaffb6e55316407&animal=dog&location=48226&sex=" + sex + "&age=" + age + "&size=" + size + "&output=full&format=json"
+                    url: "http://api.petfinder.com/pet.find?key=ad9636f9b9a6415e1aaffb6e55316407&animal=dog&location=48226&sex=" + sex + "&age=" + age + "&size=" + size + "&count=1&co&output=full&format=json"
                 }).then(function successCallback(response) {
                     console.log(response);
                 });
