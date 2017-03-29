b4d6b714fc831102e739ee0c6b53cca97ceac2e0
Adam Lezotte <adam@lezotte.me>
2017-03-28 21:26:26 -0400

    finished question arrays

--- script/controller/formctrl.js
+++ script/controller/formctrl.js
@@ -27,96 +27,114 @@ app.controller("formCtrl", ["$scope", "adoptFactory", "$location", function($sco
         	image:"",
         	imageattribution:"",
         },
-        // { //what is the user's work schedule?
-        //     number: 2,
-        // 	question:"Which of these best decribes your after-work routine?",
-        // 	answer1: {answer:"I'm my own boss! I don't need no routines!", weight: 0},
-        // 	answer2: {answer:"Cooking dinner and then watching some TV", weight: 2},
-        // 	answer3: {answer:"Happy hour!!!!!!!!!!!!!!!!!!!", weight: 5},
-        // 	answer4: {answer:"Hopping in the bed immediately! I'm beat!", weight: 6},
-        // 	image:"",
-        // 	imageattribution:"",
-        // },
-        // { //how willing is the user to train?
-        //     number: 3,
-        // 	question:"How thin is your patience?",
-        // 	answer1: {answer:"Too thin for this quiz!", weight: 5},
-        // 	answer2: {answer:"Eh, I'm kind of impatient", weight: 3},
-        // 	answer3: {answer:"I'm fairly patient", weight: 1},
-        // 	answer4: {answer:"I'm actually a really patient person!", weight: 0},
-        // 	image:"",
-        // 	imageattribution:"",
-        // },
-        // { //how often would the user walk their dog?
-        //     number: 4,
-        // 	question:"What kind of shoes do you own?",
-        // 	answer1: {answer:"Nothing but heels, sis!", weight: 5},
-        // 	answer2: {answer:"Loafers and like one pair of sneakers", weight: 3},
-        // 	answer3: {answer:"Just picked up a pair of Nike Flyknits! Dope!", weight: 1},
-        // 	answer4: {answer:"I have like 5 pairs of hiking boots", weight: 0},
-        // 	image:"",
-        // 	imageattribution:"",
-        // },
-        // { //how much yard space does the user have?
-        //     number: 5,
-        // 	question:"How fond of cutting grass are you?",
-        // 	answer1: {answer:"What grass?", weight: 0},
-        // 	answer2: {answer:"Not my favorite but I get it done", weight: 2},
-        // 	answer3: {answer:"Psh, I pay a lawncare service", weight: 4},
-        // 	answer4: {answer:"Ride-on mowers for life!", weight: 5},
-        // 	image:"",
-        // 	imageattribution:"",
-        // },
-        // { //how often does the user travel?
-        //     number: 6,
-        // 	question:"Choose a vacay:",
-        // 	answer1: {answer:"Flying out to some trendy island", weight: 3},
-        // 	answer2: {answer:"Honestly, I'm cool staying home with snacks and Hulu", weight: 0},
-        // 	answer3: {answer:"I go to my family reunion every year. Does that count?", weight: 2},
-        // 	answer4: {answer:"Camping!", weight: 1},
-        // 	image:"",
-        // 	imageattribution:"",
-        // },
-        // { //is the user disabled?
-        //     number: 7,
-        // 	question:"What are your thoughts on ableism?",
-        // 	answer1: {answer:"As a person with a disablity, I'm completely anti-ableism!", weight: 2},
-        // 	answer2: {answer:"Yea, ableism ain't cool", weight: 0},
-        // 	answer3: {answer:"I use Tumblr, so I've heard it's bad", weight: 0},
-        // 	answer4: {answer:"What's ableism? (you're probably an ableist, bruh)", weight: 0},
-        // 	image:"",
-        // 	imageattribution:"",
-        // },
-        // { //how active is the user?
-        //     number: 8,
-        // 	question:"You setting off the lunk alarm, or nah?",
-        // 	answer1: {answer:"The what???", weight: 0},
-        // 	answer2: {answer:"Huh?", weight: 1},
-        // 	answer3: {answer:"No, but I've seen some jerks do it", weight: 3},
-        // 	answer4: {answer:"Bro, do you even lift!?", weight: 4},
-        // 	image:"",
-        // 	imageattribution:"",
-        // },
-        // { //is the user an extrovert or an introvert?
-        //     number: 9,
-        // 	question:"Which Beyoncé are you?",
-        // 	answer1: {answer:"Beyoncé as Lily in Fighting Temptations", weight: 1},
-        // 	answer2: {answer:"Beyoncé as Carmen in Carmen: A Hip-Hopera", weight: 2},
-        // 	answer3: {answer:"Beyoncé as Deena Jones in Dreamgirls", weight: 0},
-        // 	answer4: {answer:"Beyoncé as her real life self", weight: 3},
-        // 	image:"",
-        // 	imageattribution:"",
-        // },
-        // { //does the user have a pet gender preference?
-        //     number: 10,
-        //   question:"Choose a Disney dog:",
-        //   answer1: {answer:"Perdita", weight: 0},
-        //   answer2: {answer:"Lady", weight: 0},
-        //   answer3: {answer:"Pongo", weight: 0},
-        //   answer4: {answer:"Tramp", weight: 0},
-        //   image:"",
-        //   imageattribution:"",
-        // }
+        { //what is the user's work schedule?
+            number: 2,
+        	question:"Which of these best decribes your after-work routine?",
+            answers: [
+                {answer:"I'm my own boss! I don't need no routines!", weight: 0},
+            	{answer:"Cooking dinner and then watching some TV", weight: 2},
+            	{answer:"Happy hour!!!!!!!!!!!!!!!!!!!", weight: 5},
+            	{answer:"Hopping in the bed immediately! I'm beat!", weight: 6}
+            ],
+        	image:"",
+        	imageattribution:"",
+        },
+        { //how willing is the user to train?
+            number: 3,
+        	question:"How thin is your patience?",
+            answers: [
+                {answer:"Too thin for this quiz!", weight: 5},
+            	{answer:"Eh, I'm kind of impatient", weight: 3},
+            	{answer:"I'm fairly patient", weight: 1},
+            	{answer:"I'm actually a really patient person!", weight: 0}
+            ],
+        	image:"",
+        	imageattribution:"",
+        },
+        { //how often would the user walk their dog?
+            number: 4,
+        	question:"What kind of shoes do you own?",
+            answers: [
+                {answer:"Nothing but heels, sis!", weight: 5},
+            	{answer:"Loafers and like one pair of sneakers", weight: 3},
+            	{answer:"Just picked up a pair of Nike Flyknits! Dope!", weight: 1},
+            	{answer:"I have like 5 pairs of hiking boots", weight: 0}
+            ],
+        	image:"",
+        	imageattribution:"",
+        },
+        { //how much yard space does the user have?
+            number: 5,
+        	question:"How fond of cutting grass are you?",
+            answers: [
+                {answer:"What grass?", weight: 0},
+            	{answer:"Not my favorite but I get it done", weight: 2},
+                {answer:"Psh, I pay a lawncare service", weight: 4},
+            	{answer:"Ride-on mowers for life!", weight: 5}
+            ],
+        	image:"",
+        	imageattribution:"",
+        },
+        { //how often does the user travel?
+            number: 6,
+        	question:"Choose a vacay:",
+            answers: [
+                {answer:"Flying out to some trendy island", weight: 3},
+            	{answer:"Honestly, I'm cool staying home with snacks and Hulu", weight: 0},
+            	{answer:"I go to my family reunion every year. Does that count?", weight: 2},
+            	{answer:"Camping!", weight: 1}
+            ],
+        	image:"",
+        	imageattribution:"",
+        },
+        { //is the user disabled?
+            number: 7,
+        	question:"What are your thoughts on ableism?",
+            answers: [
+                {answer:"As a person with a disablity, I'm completely anti-ableism!", weight: 2},
+            	{answer:"Yea, ableism ain't cool", weight: 0},
+            	{answer:"I use Tumblr, so I've heard it's bad", weight: 0},
+            	{answer:"What's ableism? (you're probably an ableist, bruh)", weight: 0}
+            ],
+        	image:"",
+        	imageattribution:"",
+        },
+        { //how active is the user?
+            number: 8,
+        	question:"You setting off the lunk alarm, or nah?",
+            answers: [
+                {answer:"The what???", weight: 0},
+            	{answer:"Huh?", weight: 1},
+            	{answer:"No, but I've seen some jerks do it", weight: 3},
+            	{answer:"Bro, do you even lift!?", weight: 4}
+            ],
+        	image:"",
+        	imageattribution:"",
+        },
+        { //is the user an extrovert or an introvert?
+            number: 9,
+        	question:"Which Beyoncé are you?",
+            answers: [
+                {answer:"Beyoncé as Lily in Fighting Temptations", weight: 1},
+            	{answer:"Beyoncé as Carmen in Carmen: A Hip-Hopera", weight: 2},
+            	{answer:"Beyoncé as Deena Jones in Dreamgirls", weight: 0},
+            	{answer:"Beyoncé as her real life self", weight: 3}
+            ],
+        	image:"",
+        	imageattribution:"",
+        },
+        { //does the user have a pet gender preference?
+            number: 10,
+          question:"Choose a Disney dog:",
+          answers: [
+            {answer:"Perdita", weight: 0},
+            {answer:"Lady", weight: 0},
+            {answer:"Pongo", weight: 0},
+            {answer:"Tramp", weight: 0}
+          ],
+          image:"",
+          imageattribution:"",
+        }
     ];
 
     $scope.activeQ = 0;
