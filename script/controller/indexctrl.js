97c4cf7fdd5940e54a63507808a611846df671b1
Adam Lezotte <adam@lezotte.me>
2017-03-30 20:40:29 -0400

    logo show/hide

--- script/controller/indexctrl.js
+++ script/controller/indexctrl.js
@@ -2,9 +2,17 @@ var app = angular.module("badaBingBadaBoom");
 
 app.controller("indexCtrl", function ($scope, adoptFactory, $location) {
 
+    $scope.hideLogo = false;
+
+    $scope.tab = function (tab, hide) {
+        $location.path(tab);
+        $scope.hideLogo =  hide;
+    }
+
     $scope.submitZip = function () {
         adoptFactory.getZip($scope.userZip);
         $location.path("/form");
+        $scope.hideLogo = true;
     }
 
 });
